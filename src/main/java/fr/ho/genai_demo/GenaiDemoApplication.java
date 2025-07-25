7b63adaecda097f6803e321d77262c1fc32c0d79
ouazzih <at.inetum.ouazzi@sesam-vitale.fr>
2025-07-25 11:14:56 +0200

    init project

---
+++ src/main/java/fr/ho/genai_demo/GenaiDemoApplication.java
@@ -0,0 +1,13 @@
+package fr.ho.genai_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenaiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenaiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/ho/genai_demo/GenaiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ho.genai_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenaiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
